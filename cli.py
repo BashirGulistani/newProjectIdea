5f417188d10d0fc25de54b88db357dac59e36189
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-01-12 23:34:25 -0500

    Update cli.py

--- cli.py
+++ cli.py
@@ -12,4 +12,13 @@ def http(method: str, path: str, api_key: str, body=None):
     headers = {"X-API-Key": api_key}
     if body is not None:
 
+    data = json.dumps(body).encode("utf-8")
+    headers["Content-Type"] = "application/json"
+    req = Request(url=url, data=data, headers=headers, method=method)
+    with urlopen(req) as res:
+        return json.loads(res.read().decode("utf-8"))
+
+
+
+
 
