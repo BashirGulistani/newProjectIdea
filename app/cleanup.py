9c58bc4642b0c89519138df7b62a308b94f789c5
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2026-01-13 23:04:17 -0500

    Create cleanup.py

---
+++ app/cleanup.py
@@ -0,0 +1,14 @@
+import asyncio
+from datetime import datetime, timezone
+
+from sqlalchemy import delete
+from sqlalchemy.orm import Session
+
+from app.db import SessionLocal
+from app.models import Signal
+
+
+
+
+
+
